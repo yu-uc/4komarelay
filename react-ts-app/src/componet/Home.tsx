5b55fdd02cf1a6b9e1c8f665b2e336cdc35e37ac
yu-uc <73521166+yu-uc@users.noreply.github.com>
2020-12-15 15:26:15 +0900

    ルーティングとログイン画面
    ルーティングのプログラムとコンポーネントのフォルダとHomeとLoginの生成

---
+++ react-ts-app/src/componet/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home =()=>{
+
+    return( 
+      <div>
+        <div><p><Link to="/login">Login</Link></p></div>   
+      </div>
+      
+    )
+  }
+
+  export default Home;
