572ad05b7361317f62f3b509d1acfdcd6c3fb3bf
yu-uc <73521166+yu-uc@users.noreply.github.com>
2020-12-19 23:49:25 +0900

    ログイン画面
    ログイン画面ui完成（仮）

--- react-ts-app/src/componet/Login.tsx
+++ react-ts-app/src/componet/Login.tsx
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { createStyles, makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { Grid, Button, TextField, Container } from "@material-ui/core";
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     button: {
       backgroundColor: "#FFB549",
       color: "#FFFFFF",
+      borderLeftColor: "#FFB549",
       marginBottom: "8px",
+      margin: theme.spacing(2),
+      width: "40ch",
+      "&:hover": {
+        backgroundColor: "#FFF",
+        color: "#FFB549",
+      },
+    },
+    root: {
+      "& .MuiTextField-root": {
+        margin: theme.spacing(1),
+        width: "40ch",
+      },
     },
   })
 );
@@ -39,29 +52,51 @@ const Login = (props: any) => {
 
   return (
     <div>
-      <div>
-        <div>
-          <label htmlFor="">ID</label>
-          <input type="text" onChange={(id) => setUserEmail(id.target.value)} />
-        </div>
-        <label htmlFor="">パスワード</label>
-        <input
-          type="password"
-          onChange={(pass) => setUserPassword(pass.target.value)}
-        />
-        <div>
-          <Button
-            className={classes.button}
-            variant="contained"
-            onClick={() => getAuthentication()}
-          >
-            ログイン
-          </Button>
-        </div>
-        <p>
-          <Link to="/">home</Link>
-        </p>
-      </div>
+      <Container maxWidth="xs">
+        <Grid container spacing={1} alignItems="center" justify="center">
+          <h1>ログイン</h1>
+        </Grid>
+        <Grid container spacing={2} alignItems="center" justify="center">
+          <form className={classes.root} noValidate autoComplete="off">
+            <div>
+              <TextField
+                id="outlined-required"
+                label="id(メールアドレス)"
+                defaultValue=""
+                variant="outlined"
+                onChange={(id) => setUserEmail(id.target.value)}
+              />
+            </div>
+            <div>
+              {" "}
+              <TextField
+                id="outlined-password-input"
+                label="パスワード"
+                type="password"
+                autoComplete="current-password"
+                variant="outlined"
+                onChange={(pass) => setUserPassword(pass.target.value)}
+              />
+            </div>
+          </form>
+        </Grid>
+        <Grid container spacing={2} alignItems="center" justify="center">
+          <div>
+            <Button
+              className={classes.button}
+              variant="contained"
+              onClick={() => getAuthentication()}
+            >
+              ログイン
+            </Button>
+          </div>
+        </Grid>
+        <Grid container spacing={4} alignItems="center" justify="center">
+          <p>
+            <Link to="/">アカウントをお持ちでない方はこちら</Link>
+          </p>
+        </Grid>
+      </Container>
     </div>
   );
 };
